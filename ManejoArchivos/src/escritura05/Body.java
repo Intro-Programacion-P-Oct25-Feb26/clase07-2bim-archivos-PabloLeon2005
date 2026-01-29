ed7906da020b84042989bb2021241f85a8cad981
PabloLeon2005 <pabloguaman2005@gmail.com>
2026-01-29 15:13:49 -0500

    COMMIT FINAL

--- ManejoArchivos/src/escritura05/Body.java
+++ ManejoArchivos/src/escritura05/Body.java
@@ -4,10 +4,42 @@
  */
 package escritura05;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Formatter;
+
 /**
  *
  * @author PC
  */
 public class Body {
-    
+
+    public static void agregarDatos(String valor, String placa) {
+       
+        String letra = placa.substring(0, 1).toUpperCase();
+        String nombreArchivo;
+
+        if (letra.equals("L")) {
+            nombreArchivo = "data/loja.txt";
+        } else if (letra.equals("P")) {
+            nombreArchivo = "data/pichincha.txt";
+        } else if (letra.equals("G")) {
+            nombreArchivo = "data/guayas.txt";
+        } else {
+            nombreArchivo = "data/otros.txt";
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(nombreArchivo, true);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            Formatter salida = new Formatter(osw);
+
+            salida.format("%s", valor);
+            
+            salida.close(); 
+
+        } catch (Exception e) {
+            System.err.println("Error al crear el archivo: ");
+        }
+    }
 }
--- ManejoArchivos/src/escritura05/Main.java
+++ ManejoArchivos/src/escritura05/Main.java
@@ -1,13 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package escritura05;
 
-/**
- *
- * @author PC
- */
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        String cadenaFinal = "";
+        
+        boolean continuar = true; 
+
+        while (continuar) {
+
+            System.out.println("Ingrese su nombre");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingrese su tipo de vehiculo");
+            String vehiculo = entrada.nextLine();
+            System.out.println("Ingrese su placa");
+            String placa = entrada.nextLine();
+
+            cadenaFinal = String.format("%s %s %s\n",
+                    nombre,
+                    vehiculo,
+                    placa);
+
+            Body.agregarDatos(cadenaFinal, placa);
+            
+            System.out.println("¿Desea ingresar otro vehículo? (si/no)");
+            String respuesta = entrada.nextLine();
+            
+            if (respuesta.equals("no")) {
+                continuar = false; 
+            }
+        }
+        
+    }
 }
